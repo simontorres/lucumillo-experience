ed6da868d702ac6c61f274ff15ce3984cda5701e
Simon Torres <simon.torres@noirlab.edu>
2025-01-05 20:30:16 -0300

    more testing

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/lucumillo-experience',
-  images: {
-    unoptimized: true
-  }
 };
 
 export default nextConfig;
