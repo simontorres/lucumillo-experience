b14ae8b31b63c8117d5f617d823033d2faef29bb
Simon Torres <simon.torres@noirlab.edu>
2025-01-05 16:03:51 -0300

    Changed image path

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,14 @@ import Image from 'next/image';
 import React from 'react';
 import Link from 'next/link';
 
-import {tours} from '../data/tours'
+import { tours } from '../data/tours'
 
 const HomePage = () => {
 
   return (
     <div className="min-h-screen flex flex-col">
-      
-      
+
+
       <main className="flex-grow container mx-auto p-4">
         <h1 className="text-4xl font-bold text-center my-8">
           Welcome to Lucumillo Experience
@@ -20,27 +20,27 @@ const HomePage = () => {
         </p>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {tours.map(tour => (
-            <Link key={ tour.id } href={`/tours/${tour.id}`}>
-            <div key={tour.id} className="rounded-lg shadow-lg overflow-hidden">
-              <Image
-                src={tour.image}
-                alt={tour.title}
-                width={480}
-                height={400}
-                className="w-full h-60 object-cover"
-              />
-              <div className="p-4">
-                <h2 className="text-2xl font-bold mb-2">{tour.title}</h2>
-                <p className="text-gray-300">{tour.description}</p>
+            <Link key={tour.id} href={`/tours/${tour.id}`}>
+              <div key={tour.id} className="rounded-lg shadow-lg overflow-hidden">
+                <Image
+                  src={`/images/tours/${tour.image}`}
+                  alt={tour.title}
+                  width={480}
+                  height={400}
+                  className="w-full h-60 object-cover"
+                />
+                <div className="p-4">
+                  <h2 className="text-2xl font-bold mb-2">{tour.title}</h2>
+                  <p className="text-gray-300">{tour.description}</p>
+                </div>
               </div>
-              </div>
-              </Link>
-            
+            </Link>
+
           ))}
         </div>
       </main>
 
-      
+
     </div>
   );
 };
--- app/tours/[id]/page.tsx
+++ app/tours/[id]/page.tsx
@@ -5,15 +5,15 @@ import { tours } from '../../../data/tours'
 import Link from 'next/link';
 
 export async function generateStaticParams() {
-  return tours.map((tour) => ({ id: tour.id}))
+  return tours.map((tour) => ({ id: tour.id }))
 }
 
 export default async function Tours({ params, }: { params: Promise<{ id: string }> }) {
-  
+
   const id = (await params).id
 
   const tour = tours.find((tour) => tour.id === id)
-  
+
   if (!tour) {
     return <p>Not found!</p>
   }
@@ -25,19 +25,19 @@ export default async function Tours({ params, }: { params: Promise<{ id: string 
           <Link href='/'>Back to Tours</Link>
         </div>
         <h1 className="text-4xl font-bold text-center my-8">
-          { tour.title }
+          {tour.title}
         </h1>
         <p className="text-center text-gray-300 mb-8">
           {tour.description}
         </p>
         <div className='flex justify-center'>
-        <Image
-          src={tour.image}
-          alt={tour.title}
-          width={480}
-          height={600}
-          className="w-full object-cover"
-        />
+          <Image
+            src={`/images/tours/${tour.image}`}
+            alt={tour.title}
+            width={480}
+            height={600}
+            className="w-full object-cover"
+          />
         </div>
       </main>
     </div>
