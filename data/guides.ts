10e2772f27a148e8b9e0219be1c1616baf4ef666
Simon Torres <simon.torres@noirlab.edu>
2025-01-10 19:13:10 -0300

    Update guide bios

--- data/guides.ts
+++ data/guides.ts
@@ -10,13 +10,13 @@ export const guides: Guide[] = [
     {
       id: '2',
       name: "Mauricio Rojas",
-      bio: "Engineer",
+      bio: "Mauricio combines his technical expertise with a deep love for nature and adventure. Having worked at a professional observatory as a Laser Operator, he gained a unique perspective on the night sky, which he loves to share with others. Now an Optical Technician, he continues to explore the wonders of light and vision. Outside of his technical work, Mauricio is an avid bird photographer with a sharp eye for capturing the beauty of nature. As an experienced mountaineer, he has climbed peaks over 6,400 meters (21,000 feet) above sea level, offering stories of adventure and breathtaking landscapes. Mauricio’s enthusiasm for the natural world and the stars above makes him a captivating guide for explorers and stargazers alike.",
       picture: '',
     },
     {
       id: '3',
       name: "Simón Torres",
-      bio: "Engineer",
+      bio: "With a background in physics and a specialization in astronomy, Simón offers a unique perspective on the universe and the natural world. Having conducted research in astrophysics and pursued advanced studies in the field, he transitioned into software engineering, combining analytical skills with creative problem-solving. Beyond his professional pursuits, Simón is an avid explorer of Chile’s wild landscapes, enjoying camping under star-filled skies and capturing stunning photographs of both birds and the night sky. Recently, he has taken up ham radio operation, a technical hobby that fuels his curiosity and passion for communication. With a love for nature, the stars, and discovery, Simón brings a sense of wonder and insight to every journey through Chile’s breathtaking scenery and celestial wonders.",
       picture: '',
     }
   ]
