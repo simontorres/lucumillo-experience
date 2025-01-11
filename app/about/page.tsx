09a76a950d5d4c14102b18466258aec36e122738
Simon Torres <simon.torres@noirlab.edu>
2025-01-10 23:28:30 -0300

    Update lucumillos rationale

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -9,28 +9,36 @@ const About = () => {
       
       
       <main className="flex-grow container mx-auto p-4">
-        <h1 className="text-3xl font-bold my-8">
+        <h1 className="text-center text-3xl font-bold my-8">
           Lucumillo Experience
         </h1>
-        <p className="text-gray-300 mb-8">
+        <p className="text-center text-gray-300 mb-8">
           We offer personalized tours to whale watching and to see the night sky in northern Chile.
         </p>
         <div className='mb-8'>
-          <h2 className='text-2xl font-bold'>Why Lucumillo?</h2>
-          <p className='text-lg text-gray-300'>Lucumillo (Myrcianthes coquimbensis) is an endemic bush 
-            classified as <b>Endangered</b> its distribution comprises only a few 
-            hundred kilometers.
+          <h2 className='text-center text-2xl font-bold'>Why Lucumillo?</h2>
+          <p className='text-center text-lg text-gray-300'>
+            The name Lucumillo comes from Myrcianthes coquimbensis, a rare and endangered
+            bush native to the coastal deserts of Chile. Its narrow distribution spans
+            only a few hundred kilometers in the Coquimbo region, where it grows among
+            large granite boulders, nourished by cool, moist breezes from the Pacific Ocean.
+            The Lucumillo bush is deeply tied to this unique ecosystem,
+            relying on specific environmental conditions and facing threats from
+            habitat loss due to urban development and tourism.
           </p>
-          <p className='text-lg text-gray-300'> By naming us this way, we want to honor the resilience of this bush, also 
-          we aim to help protecting it by raising awareness about its fragility to its ever reducing habitat.
-
+          <p className='text-center text-lg text-gray-300'>
+            We chose this name to honor the resilience of the Lucumillo, which,
+            despite its fragility, thrives in the harsh conditions of the Chilean coastal desert.
+            By adopting this name, we hope to raise awareness about the importance of
+            conserving both the species and its delicate habitat. In doing so, we aim to contribute to
+            the protection of this remarkable part of Chile’s natural heritage.
           </p>
         </div>
 
         {/* OUR GUIDES */}
 
         <div className='mb-8'>
-          <h2 className='text-2xl font-bold'>Our Guides</h2>
+          <h2 className='text-center text-2xl font-bold'>Our Guides</h2>
           <p>We primarily collaborate with local, specialized guides provided by
             the sites we visit, ensuring you benefit from authentic, insider
             knowledge. Additionally, our team includes guides with diverse
@@ -43,8 +51,8 @@ const About = () => {
             guides.map(guide => (
             <div key={guide.id} className='rounded-lg shadow-lg overflow-hidden'>
               <div className="p-4">
-                <h2 className="text-2xl font-bold mb-2">{guide.name}</h2>
-                <p className="text-gray-300">{guide.bio}</p>
+                  <h2 className="text-center text-2xl font-bold mb-2">{guide.name}</h2>
+                  <p className="text-center text-gray-300">{guide.bio}</p>
               </div>
             </div>
             ))
