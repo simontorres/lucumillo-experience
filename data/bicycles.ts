bc801cee79e9da5db943f0c696de063dfe10dd23
Simon Torres <simon.torres@noirlab.edu>
2025-02-14 17:14:19 -0300

    reformated files to make it easier to translate into another language

--- data/bicycles.ts
+++ data/bicycles.ts
@@ -8,8 +8,18 @@ export const bicycles: BicycleRental =
     },
     price: 2500,
     gallery: [
-        { src: "bicycles_001.webp", alt: { en: "Somewhere in La Serena", es: "En algún lugar de La Serena" } },
-        { src: "bicycles_002.webp", alt: { en: "On Francisco de Aguirre Avenue", es: "En la avenida Francisco de Aguirre" } }
+        {
+            src: "bicycles_001.webp", alt: {
+                en: "Somewhere in La Serena",
+                es: "En algún lugar de La Serena"
+            }
+        },
+        {
+            src: "bicycles_002.webp", alt: {
+                en: "On Francisco de Aguirre Avenue",
+                es: "En la avenida Francisco de Aguirre"
+            }
+        }
     ],
     retrieving_place: "Hostal Luna Del Mar, Isabel Riquelme 840, La Serena",
     enabled: true
--- data/services.ts
+++ data/services.ts
@@ -13,14 +13,62 @@ export const services: Service[] = [
     },
     path: "tours",
     gallery: [
-      { src: "/images/services/xs/nightsky_001.webp", alt: { en: "The south celestial pole photographed over three hours.", es: "El polo sur celeste fotografiado durante tres horas." } },
-      { src: "/images/services/xs/nightsky_002.webp", alt: { en: "Stunning view of the Milky Way from Elqui Valley.", es: "Impresionante vista de la Vía Láctea desde el Valle de Elqui." } },
-      { src: "/images/services/xs/nightsky_003_2.webp", alt: { en: "Telescope under a starry sky.", es: "Telescopio bajo un cielo estrellado." } },
-      { src: "/images/services/xs/whales_003.webp", alt: { en: "Humpback whale breaching near the coast.", es: "Ballena jorobada saltando cerca de la costa." } },
-      { src: "/images/services/xs/whales_004.webp", alt: { en: "A whale fluke diving into the ocean.", es: "Una cola de ballena sumergiéndose en el océano." } },
-      { src: "/images/services/xs/elqui_valley_001.webp", alt: { en: "Montegrande, home of Nobel laureate Gabriela Mistral.", es: "Montegrande, hogar de la premio Nobel Gabriela Mistral." } },
-      { src: "/images/services/xs/birdwatching_003.webp", alt: { en: "Tufted Tit-Tyrant perching on an old branch.", es: "Titira de cresta blanca posada en una rama vieja." } },
-      { src: "/images/services/xs/something_else_002.webp", alt: { en: "Beautiful sunset in La Serena with Coquimbo in the background.", es: "Hermoso atardecer en La Serena con Coquimbo de fondo." } }
+      {
+        src: "/images/services/xs/nightsky_001.webp",
+        alt: {
+          en: "The south celestial pole photographed over three hours.",
+          es: "El polo sur celeste fotografiado durante tres horas."
+        }
+      },
+      {
+        src: "/images/services/xs/nightsky_002.webp",
+        alt: {
+          en: "Stunning view of the Milky Way from Elqui Valley.", 
+          es: "Impresionante vista de la Vía Láctea desde el Valle de Elqui."
+        }
+      },
+      {
+        src: "/images/services/xs/nightsky_003_2.webp",
+        alt: {
+          en: "Telescope under a starry sky.", 
+          es: "Telescopio bajo un cielo estrellado."
+        }
+      },
+      {
+        src: "/images/services/xs/whales_003.webp",
+        alt: {
+          en: "Humpback whale breaching near the coast.", 
+          es: "Ballena jorobada saltando cerca de la costa."
+        }
+      },
+      {
+        src: "/images/services/xs/whales_004.webp",
+        alt: {
+          en: "A whale fluke diving into the ocean.", 
+          es: "Una cola de ballena sumergiéndose en el océano."
+        }
+      },
+      {
+        src: "/images/services/xs/elqui_valley_001.webp",
+        alt: {
+          en: "Montegrande, home of Nobel laureate Gabriela Mistral.", 
+          es: "Montegrande, hogar de la premio Nobel Gabriela Mistral."
+        }
+      },
+      {
+        src: "/images/services/xs/birdwatching_003.webp",
+        alt: {
+          en: "Tufted Tit-Tyrant perching on an old branch.", 
+          es: "Titira de cresta blanca posada en una rama vieja."
+        }
+      },
+      {
+        src: "/images/services/xs/something_else_002.webp",
+        alt: {
+          en: "Beautiful sunset in La Serena with Coquimbo in the background.", 
+          es: "Hermoso atardecer en La Serena con Coquimbo de fondo."
+        }
+      }
     ],
     enabled: true,
   },
@@ -36,8 +84,20 @@ export const services: Service[] = [
     },
     path: "arriendo-de-bicicletas",
     gallery: [
-      { src: "/images/services/xs/bicycles_001.webp", alt: { en: "Mountain bikes ready for an adventure.", es: "Bicicletas de montaña listas para la aventura." } },
-      { src: "/images/services/xs/bicycles_002.webp", alt: { en: "Cycling through scenic landscapes.", es: "Pedaleando por paisajes escénicos." } }
+      {
+        src: "/images/services/xs/bicycles_001.webp",
+        alt: {
+          en: "Mountain bikes ready for an adventure.",
+          es: "Bicicletas de montaña listas para la aventura."
+        }
+      },
+      {
+        src: "/images/services/xs/bicycles_002.webp",
+        alt: {
+          en: "Cycling through scenic landscapes.",
+          es: "Pedaleando por paisajes escénicos."
+        }
+      }
     ],
     enabled: true,
 },
