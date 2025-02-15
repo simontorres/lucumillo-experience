c1e9c573c3e719bae0673808f0dd92fa9671aa8f
Simon Torres <simon.torres@noirlab.edu>
2025-02-15 13:54:31 -0300

    add german and french tranlations

--- data/bicycles.ts
+++ data/bicycles.ts
@@ -4,20 +4,28 @@ export const bicycles: BicycleRental =
 {
     includes: {
         en: "27-inch wheel bicycle, helmet, lock, and lights.",
-        es: "Bicicleta aro 27, casco, candado y luces."
+        es: "Bicicleta aro 27, casco, candado y luces.",
+        de: "27-Zoll-Fahrrad, Helm, Schloss und Lichter.",
+        fr: "Vélo avec roues de 27 pouces, casque, cadenas et lumières."
     },
     price: 2500,
     gallery: [
         {
-            src: "bicycles_001.webp", alt: {
+            src: "bicycles_001.webp", 
+            alt: {
                 en: "Somewhere in La Serena",
-                es: "En algún lugar de La Serena"
+                es: "En algún lugar de La Serena",
+                de: "Irgendwo in La Serena",
+                fr: "Quelque part à La Serena"
             }
         },
         {
-            src: "bicycles_002.webp", alt: {
+            src: "bicycles_002.webp", 
+            alt: {
                 en: "On Francisco de Aguirre Avenue",
-                es: "En la avenida Francisco de Aguirre"
+                es: "En la avenida Francisco de Aguirre",
+                de: "Auf der Avenida Francisco de Aguirre",
+                fr: "Sur l'avenue Francisco de Aguirre"
             }
         }
     ],
--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import createMiddleware from "next-intl/middleware";
 
 const intlMiddleware = createMiddleware({
-  locales: ["en", "es"],
+  locales: ["en", "es", "de", "fr"],
   defaultLocale: "es",
 });
 
@@ -10,9 +10,13 @@ export function middleware(req: NextRequest) {
   const pathname = req.nextUrl.pathname;
 
   // Rewrite static files to remove locale prefix
-  if (pathname.startsWith("/en/_next/") || pathname.startsWith("/es/_next/") || pathname.endsWith(".webp"))  {
+    if (pathname.startsWith("/en/_next/")
+        || pathname.startsWith("/es/_next/")
+        || pathname.startsWith("/de/_next/")
+        || pathname.startsWith("/fr/_next/")
+        || pathname.endsWith(".webp")) {
     return NextResponse.rewrite(
-      new URL(pathname.replace(/^\/(en|es)/, ""), req.url)
+      new URL(pathname.replace(/^\/(en|es|de|fr)/, ""), req.url)
     );
   }
 
